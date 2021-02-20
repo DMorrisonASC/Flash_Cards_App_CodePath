bd0fd10fe3a23226562b807708a8109efe3f0deb
Daeshaun <daeshauncodes@gmail.com>
2021-02-20 14:55:38 -0500

    Update

--- app/src/main/java/com/example/flash_cards_app/MainActivity.java
+++ app/src/main/java/com/example/flash_cards_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flash_cards_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView questionTextView = findViewById(R.id.questionTextView);
+        TextView answerTextView = findViewById(R.id.answerTextView);
+
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(answerTextView.VISIBLE);
+            }
+        });
     }
 }
